8c476469f98c83d113e11a510be89ad6d6963950
rickywo <rickywo@gmail.com>
2017-04-15 21:47:59 +1000

    DAO

---
+++ source/models/model_dao.py
@@ -0,0 +1,17 @@
+
+class UserDAO:
+
+    __db = None
+
+    def __init__(self, db):
+        self.__db = db
+        pass
+
+    def save_user(self, user):
+        pass
+
+    def save_all_users(self):
+        pass
+
+    def get_all_users(self):
+        pass
